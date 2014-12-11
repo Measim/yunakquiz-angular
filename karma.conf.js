e69add9e6ac4aaf38c0bdcdf1e93be9e649b5753
Measim <l_eugene@inbox.ru>
2014-12-11 22:29:07 +0200

    karma tests commented (not used)

--- karma.conf.js
+++ karma.conf.js
@@ -1,35 +1,35 @@
-module.exports = function(config){
-  config.set({
+// module.exports = function(config){
+//   config.set({
 
-    basePath : './',
+//     basePath : './',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/components/**/*.js',
-      'app/modules/assessments/assessments.js',
-      'app/modules/users/users.js',
-      'app/modules/**/*.js'
-    ],
+//     files : [
+//       'app/bower_components/angular/angular.js',
+//       'app/bower_components/angular-route/angular-route.js',
+//       'app/bower_components/angular-mocks/angular-mocks.js',
+//       'app/components/**/*.js',
+//       'app/modules/assessments/assessments.js',
+//       'app/modules/users/users.js',
+//       'app/modules/**/*.js'
+//     ],
 
-    autoWatch : true,
+//     autoWatch : true,
 
-    frameworks: ['jasmine'],
+//     frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+//     browsers : ['Chrome'],
 
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
+//     plugins : [
+//             'karma-chrome-launcher',
+//             'karma-firefox-launcher',
+//             'karma-jasmine',
+//             'karma-junit-reporter'
+//             ],
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+//     junitReporter : {
+//       outputFile: 'test_out/unit.xml',
+//       suite: 'unit'
+//     }
 
-  });
-};
+//   });
+// };
